fc59c0e99d5f502a28ff245ec2972ae2bd1d922d
Nicholas Mirigliani <36427716+NobbyBop@users.noreply.github.com>
2025-01-14 14:49:03 -0500

    basic

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="header">
+        <h1>Nicholas Mirigliani</h1>
+        <h2>Developer. Artist. Third Thing.</h2>
+        <a href="https://www.linkedin.com/in/nicholas-mirigliani/">LinkedIn</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
