54021c1351d8cd97a2344d90b3764db322df4249
Nicholas Mirigliani <36427716+NobbyBop@users.noreply.github.com>
2025-01-23 20:48:02 -0500

    starting some fun styling

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -10,13 +10,36 @@ function Landing() {
     "son of a gun...",
   ];
   let subtitle = subtitles[Math.floor(Math.random() * subtitles.length)];
+  let scrollTextA =
+    "WELCOME. ARE YOU READY? THE TIME IS NOW. DON'T BE AFRAID. A WHOLE WORLD LIES AHEAD.";
+  let scrollTextB =
+    "YOU'RE LATE. GET A MOVE ON. WHAT ARE YOU WAITING FOR? THE WORLD WILL CRUMBLE AT YOUR FEET.";
   return (
     <div className="landingContainer">
+      <h1 className="scrolling-text">
+        <div className="scrolling-text-innerA">
+          {scrollTextA}&nbsp;{scrollTextA}
+        </div>
+      </h1>
       <div className="landingContents">
+        {/* <div className="scroller">
+          <h1 className="scroll-text">
+            WELCOME. YOU ARE EARLY. THE NEXT BIG THING. WELCOME. YOU ARE EARLY.
+            THE NEXT BIG THING. WELCOME. YOU ARE EARLY. THE NEXT BIG THING.
+            WELCOME. YOU ARE EARLY. THE NEXT BIG THING. WELCOME. YOU ARE EARLY.
+            THE NEXT BIG THING. WELCOME. YOU ARE EARLY. THE NEXT BIG THING.
+          </h1>
+        </div> */}
+
         <h1 className="landingTitle">Nicholas Mirigliani</h1>
         <p className="landingSubtitle">{subtitle}</p>
         <Navigation />
       </div>
+      <h1 className="scrolling-text">
+        <div className="scrolling-text-innerB">
+          {scrollTextB}&nbsp;{scrollTextB}
+        </div>
+      </h1>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,19 @@
 import { NavLink } from "react-router-dom";
 import Navigation from "./Navigation";
 function NavBar() {
+  let scrollText =
+    "WHAT WILL YOU FIND? WHAT WILL YOU THINK? WHAT WILL YOU FEEL? WHAT WILL YOU LEARN? YOU'RE HERE. WHAT WILL YOU FIND? WHAT WILL YOU THINK? WHAT WILL YOU FEEL? WHAT WILL YOU LEARN?";
   return (
     <div className="navContainer">
       <NavLink className="headerNavLink" to="/">
         <h1>Nicholas Mirigliani</h1>
       </NavLink>
       <Navigation />
+      <p className="scrolling-textB">
+        <div className="scrolling-text-innerA">
+          {scrollText}&nbsp;{scrollText}
+        </div>
+      </p>
     </div>
   );
 }
