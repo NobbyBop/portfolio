7e236328674186650597b228d2bf9b7052f8e5a5
Nicholas Mirigliani <36427716+NobbyBop@users.noreply.github.com>
2025-01-30 18:58:14 -0500

    typos and rephrasing

--- src/components/Art/Mobius/Art_Mobius.jsx
+++ src/components/Art/Mobius/Art_Mobius.jsx
@@ -56,14 +56,15 @@ function Art_Mobius() {
           src={`${import.meta.env.BASE_URL}mobius/croppedbeatles.png`}
         />
         <p>
-          MOBIUS LINE is a ARG (Augmented Reality Game) which I co-created with
-          Sumner Badzik. It is best experienced to be played. Players would have
-          posters around their neighborhood, which links to the start of the
-          game. This project invovled creating branding and marketing for our
-          fictitious band MOBIUS LINE, as well as crafting a narrative. On this
-          page I have included some of the graphic design work I did for the
-          project, as well as some of our band photos, taken by Andrew
-          Yurovchak. I hope you'll take a little time to explore the game{" "}
+          MOBIUS LINE is a ARG (Augmented Reality Game) that I co-created with
+          Sumner Badzik. This project involved creating branding and marketing
+          for our fictitious band MOBIUS LINE, as well as constructing a
+          narrative which is scattered across multiple social media platforms
+          and accounts. Originally, posters (seen below) were hung around the
+          neighborbood, linking to the start of the game.On this page I have
+          included some of the graphic design work I did for the project, as
+          well as some of our band photos, taken by Andrew Yurovchak. I hope
+          you'll take a little time to explore the game{" "}
           <a target="_blank" href="https://m.youtube.com/watch?v=VJmVcI-avlE">
             here
           </a>
