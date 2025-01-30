86b011c4ed102ee64bc33d3387ee281b7dcf4696
Nicholas Mirigliani <36427716+NobbyBop@users.noreply.github.com>
2025-01-30 18:54:02 -0500

    mobius art page

--- src/components/Art/Mobius/Art_Mobius.jsx
+++ src/components/Art/Mobius/Art_Mobius.jsx
@@ -2,6 +2,44 @@ import NavBar from "../../NavBar";
 function Art_Mobius() {
   const body = document.body;
   body.className = "default-bg";
+  const items = [
+    {
+      id: 2,
+      title: "Mercury Lounge Poster",
+      image: `${import.meta.env.BASE_URL}mobius/Mercury_Lounge.jpg`,
+      comment: "",
+    },
+    {
+      id: 3,
+      title: "Tractor Tavern Poster",
+      image: `${import.meta.env.BASE_URL}mobius/Tractor_Tavern.jpg`,
+      comment: "",
+    },
+    {
+      id: 4,
+      title: "Grey Eagle Poster",
+      image: `${import.meta.env.BASE_URL}mobius/Grey_Eagle.jpg`,
+      comment: "",
+    },
+    {
+      id: 1,
+      title: "QR Code",
+      image: `${import.meta.env.BASE_URL}mobius/QR.jpg`,
+      comment: "",
+    },
+    {
+      id: 5,
+      title: "Promotional Image 1",
+      image: `${import.meta.env.BASE_URL}mobius/elevator.png`,
+      comment: "",
+    },
+    {
+      id: 6,
+      title: "Promotional Image 4",
+      image: `${import.meta.env.BASE_URL}mobius/climb.png`,
+      comment: "",
+    },
+  ];
 
   return (
     <div className="sketchOuterContainer">
@@ -9,23 +47,39 @@ function Art_Mobius() {
         <NavBar scrollText="HAVE YOU HEARD THIS SONG? HAVE YOU HEARD THIS SONG? HAVE YOU HEARD THIS SONG? HAVE YOU HEARD THIS SONG?" />
       </div>
       <div className="sketchTitle">
-        <h2>MOBIUS LINE</h2>
+        <img
+          className="mobiusArtPageTitle"
+          src={`${import.meta.env.BASE_URL}mobius/Logo2.jpg`}
+        />
+        <img
+          className="mobiusFooter"
+          src={`${import.meta.env.BASE_URL}mobius/croppedbeatles.png`}
+        />
+        <p>
+          MOBIUS LINE is a ARG (Augmented Reality Game) which I co-created with
+          Sumner Badzik. It is best experienced to be played. Players would have
+          posters around their neighborhood, which links to the start of the
+          game. This project invovled creating branding and marketing for our
+          fictitious band MOBIUS LINE, as well as crafting a narrative. On this
+          page I have included some of the graphic design work I did for the
+          project, as well as some of our band photos, taken by Andrew
+          Yurovchak. I hope you'll take a little time to explore the game{" "}
+          <a target="_blank" href="https://m.youtube.com/watch?v=VJmVcI-avlE">
+            here
+          </a>
+          .
+        </p>
       </div>
-      <div className="sketchContainer"></div>
-      <p>
-        MOBIUS LINE is a ARG (Augmented Reality Game) which I co-created with
-        Sumner Badzik. It is best experienced to be played. Players would have
-        posters around their neighborhood, which links to the start of the game.
-        This project invovled creating branding and marketing for our fictitious
-        band MOBIUS LINE, as well as crafting a narrative. On this page I have
-        included some of the graphic design work I did for the project, as well
-        as some of our band photos, taken by Andrew Yurovchak. I hope you'll
-        take a little time to explore the game{" "}
-        <a target="_blank" href="https://m.youtube.com/watch?v=VJmVcI-avlE">
-          here
-        </a>
-        .
-      </p>
+
+      <div className="sketchContainer">
+        {items.map((project) => (
+          <div className="sketchDiv" key={project.id}>
+            <img className="sketchImage" src={project.image} />
+            <h2>{project.title}</h2>
+            <p>{project.comment}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
