83dae95b555ce7c3a9a79502215e3ba4a52ee2be
Nicholas Mirigliani <nicholasmirigliani@gmail.com>
2026-09-16 22:35:53 -0400

    Fix custom domain Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/* global process */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from '@tailwindcss/vite'
@@ -5,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolio/",
+  base: process.env.VITE_PAGES_BASE || "/portfolio/",
 });
